404e4ba32a319255c90dfd8986c8d47a495270a2
ckia <1428486149@qq.com>
2019-01-26 22:32:44 +0800

    服务创建

---
+++ src/main/java/ckia/initserver/com/ConfigcenterApplication.java
@@ -0,0 +1,14 @@
+package ckia.initserver.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigcenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigcenterApplication.class, args);
+	}
+
+}
+
